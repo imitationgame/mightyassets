57853f0668f1c93b0bc8183202f6e72b2c3c449a
vauxhall <zero.griffin@gmail.com>
2016-04-24 21:46:18 -0500

    model add item position iphone

---
+++ mightyassets/sourceteq/source/m/add/madditempositioniphone.h
@@ -0,0 +1,13 @@
+//
+//  madditempositioniphone.h
+//  mightyassets
+//
+//  Created by zero on 4/24/16.
+//  Copyright © 2016 iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface madditempositioniphone : NSObject
+
+@end
---
+++ mightyassets/sourceteq/source/m/add/madditempositioniphone.m
@@ -0,0 +1,13 @@
+//
+//  madditempositioniphone.m
+//  mightyassets
+//
+//  Created by zero on 4/24/16.
+//  Copyright © 2016 iturbide. All rights reserved.
+//
+
+#import "madditempositioniphone.h"
+
+@implementation madditempositioniphone
+
+@end
