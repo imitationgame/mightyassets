f2b56e29784a44b483756e0b60483ab724e8ceac
vauxhall <zero.griffin@gmail.com>
2016-04-25 13:21:45 -0500

    views

--- mightyassets/sourceteq/source/v/add/vaddcellscreensadd.m
+++ mightyassets/sourceteq/source/v/add/vaddcellscreensadd.m
@@ -1,5 +1,44 @@
 #import "vaddcellscreensadd.h"
+#import "uicolor+uicolormain.h"
+#import "uifont+uifontmain.h"
 
 @implementation vaddcellscreensadd
+
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor second]];
+    
+    return self;
+}
+
+#pragma mark functionality
+
+-(void)setSelected:(BOOL)selected
+{
+    [super setSelected:selected];
+    [self hover];
+}
+
+-(void)setHighlighted:(BOOL)highlighted
+{
+    [super setHighlighted:highlighted];
+    [self hover];
+}
+
+#pragma mark functionality
+
+-(void)hover
+{
+    if(self.isSelected || self.isHighlighted)
+    {
+        [self setAlpha:0.2];
+    }
+    else
+    {
+        [self setAlpha:1];
+    }
+}
 
 @end
--- mightyassets/sourceteq/source/v/add/vaddcellscreensedit.m
+++ mightyassets/sourceteq/source/v/add/vaddcellscreensedit.m
@@ -1,5 +1,44 @@
 #import "vaddcellscreensedit.h"
+#import "uifont+uifontmain.h"
+#import "uicolor+uicolormain.h"
 
 @implementation vaddcellscreensedit
+
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor clearColor]];
+    
+    return self;
+}
+
+#pragma mark functionality
+
+-(void)setSelected:(BOOL)selected
+{
+    [super setSelected:selected];
+    [self hover];
+}
+
+-(void)setHighlighted:(BOOL)highlighted
+{
+    [super setHighlighted:highlighted];
+    [self hover];
+}
+
+#pragma mark functionality
+
+-(void)hover
+{
+    if(self.isSelected || self.isHighlighted)
+    {
+        [self setAlpha:0.2];
+    }
+    else
+    {
+        [self setAlpha:1];
+    }
+}
 
 @end
