0e7b067977a86e9d61695d07a8320d33d3d879cf
vauxhall <zero.griffin@gmail.com>
2016-05-01 15:38:39 -0500

    header color

--- mightyassets/sourceteq/source/v/addcoloradd/vaddcoloraddheader.h
+++ mightyassets/sourceteq/source/v/addcoloradd/vaddcoloraddheader.h
@@ -1,7 +1,12 @@
 #import <UIKit/UIKit.h>
+#import "maddcoloradd.h"
 
 @interface vaddcoloraddheader:UICollectionReusableView
 
+-(void)config:(maddcoloradd*)model;
+-(void)refresh;
+
+@property(weak, nonatomic)maddcoloradd *model;
 @property(weak, nonatomic)UIImageView *circle;
 
 @end
--- mightyassets/sourceteq/source/v/addcoloradd/vaddcoloraddheader.m
+++ mightyassets/sourceteq/source/v/addcoloradd/vaddcoloraddheader.m
@@ -1,5 +1,7 @@
 #import "vaddcoloraddheader.h"
 
+static CGFloat const colordivider = 255;
+static CGFloat const animationduration = 0.3;
 static NSInteger const circlesize = 85;
 
 @implementation vaddcoloraddheader
@@ -24,6 +26,7 @@ static NSInteger const circlesize = 85;
     [circle setContentMode:UIViewContentModeScaleAspectFit];
     [circle setImage:[[UIImage imageNamed:@"add_circlebig"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
     [circle setClipsToBounds:YES];
+    [circle setTintColor:[UIColor whiteColor]];
     self.circle = circle;
     
     [circlebase addSubview:circle];
@@ -45,6 +48,41 @@ static NSInteger const circlesize = 85;
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-1-[circle]-1-|" options:0 metrics:metrics views:views]];
     
     return self;
+}
+
+#pragma mark public
+
+-(void)config:(maddcoloradd*)model
+{
+    self.model = model;
+    [self refresh];
+}
+
+-(void)refresh
+{
+    __weak typeof(self) welf = self;
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0),
+                   ^
+                   {
+                       NSInteger red = welf.model.red.value;
+                       NSInteger green = welf.model.green.value;
+                       NSInteger blue = welf.model.blue.value;
+                       CGFloat floatred = red / colordivider;
+                       CGFloat floatgreen = green / colordivider;
+                       CGFloat floatblue = blue / colordivider;
+                       UIColor *color = [UIColor colorWithRed:floatred green:floatgreen blue:floatblue alpha:1];
+                       
+                       dispatch_async(dispatch_get_main_queue(),
+                                      ^
+                                      {
+                                          [UIView animateWithDuration:animationduration animations:
+                                           ^
+                                           {
+                                               [welf.circle setTintColor:color];
+                                           }];
+                                      });
+                   });
 }
 
 @end
