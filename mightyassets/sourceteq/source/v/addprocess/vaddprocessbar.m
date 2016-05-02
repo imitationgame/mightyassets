fd96e44cf950725024f1adfb0a878b29cfce6c10
vauxhall <zero.griffin@gmail.com>
2016-05-02 10:28:14 -0500

    please wait in processing view

--- mightyassets/sourceteq/source/v/addprocess/vaddprocessbar.m
+++ mightyassets/sourceteq/source/v/addprocess/vaddprocessbar.m
@@ -12,13 +12,24 @@
     [self setUserInteractionEnabled:NO];
     [self setTranslatesAutoresizingMaskIntoConstraints:NO];
     
+    NSDictionary *dictitle = @{NSFontAttributeName:[UIFont boldsize:18]};
+    NSDictionary *dicdescr = @{NSFontAttributeName:[UIFont regularsize:14]};
+    
+    NSAttributedString *attrtitle = [[NSAttributedString alloc] initWithString:NSLocalizedString(@"addprocess_title", nil) attributes:dictitle];
+    NSAttributedString *attrdescr = [[NSAttributedString alloc] initWithString:NSLocalizedString(@"addprocess_descr", nil) attributes:dicdescr];
+    
+    NSMutableAttributedString *mut = [[NSMutableAttributedString alloc] init];
+    [mut appendAttributedString:attrtitle];
+    [mut appendAttributedString:attrdescr];
+    
     UILabel *label = [[UILabel alloc] init];
     [label setTranslatesAutoresizingMaskIntoConstraints:NO];
     [label setUserInteractionEnabled:NO];
     [label setFont:[UIFont regularsize:20]];
     [label setTextAlignment:NSTextAlignmentCenter];
     [label setTextColor:[UIColor whiteColor]];
-    [label setText:NSLocalizedString(@"addprocess_title", nil)];
+    [label setNumberOfLines:0];
+    [label setAttributedText:mut];
     
     [self addSubview:label];
     
@@ -26,7 +37,7 @@
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[label]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[label(40)]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[label(55)]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
